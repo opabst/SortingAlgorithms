d730b5d56a11a69f0ed73db51bba4f29b5f9932f
Oliver Pabst <oliver.pabst@mailbox.org>
2018-12-11 13:51:28 +0100

    Buildskript und Quellen hinzugefügt

---
+++ src/main/java/de/oliverpabst/mergesort/MergeSort.java
@@ -0,0 +1,73 @@
+package de.oliverpabst.mergesort;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+public class MergeSort<T extends Comparable<T>>  {
+
+    private ArrayDeque<T> sortedSequence;
+
+    public MergeSort(ArrayList<T> _unsortedSequence) {
+
+        sortedSequence = sort(new ArrayDeque<>(_unsortedSequence));
+    }
+
+    public ArrayDeque<T> getSortedSequence() {
+        return sortedSequence;
+    }
+
+    public ArrayList<T> getSortedSequenceAsArrayList() {
+        return new ArrayList(sortedSequence);
+    }
+
+    private ArrayDeque<T> sort(ArrayDeque<T> _unsortedSequence) {
+        ArrayDeque<T> sortedSequence = new ArrayDeque<>();
+
+        int n = _unsortedSequence.size();
+
+        if(n >= 2) {
+            ArrayDeque<T> leftSeq = new ArrayDeque<>();
+
+            for(int i = 0; i < n/2; i++) {
+                leftSeq.addLast(_unsortedSequence.pollFirst());
+            }
+
+            ArrayDeque<T> rightSeq = new ArrayDeque<>();
+
+            for(int j = n/2; j < n; j++) {
+                rightSeq.addLast(_unsortedSequence.pollFirst());
+            }
+
+            leftSeq = sort(leftSeq);
+            rightSeq = sort(rightSeq);
+
+            sortedSequence = merge(leftSeq, rightSeq);
+
+            return sortedSequence;
+        } else {
+            return _unsortedSequence;
+        }
+    }
+
+    private ArrayDeque<T> merge(ArrayDeque<T> _leftSeq, ArrayDeque<T> _rightSeq) {
+        ArrayDeque<T> sortedSequence = new ArrayDeque<>();
+
+        while(!_leftSeq.isEmpty() && !_rightSeq.isEmpty()) {
+            if(_leftSeq.getFirst().compareTo(_rightSeq.getFirst()) <= 0) {
+                sortedSequence.addLast(_leftSeq.pollFirst());
+            } else {
+                sortedSequence.add(_rightSeq.pollFirst());
+            }
+        }
+
+        while(!_rightSeq.isEmpty()) {
+            sortedSequence.addLast(_rightSeq.pollFirst());
+        }
+
+        while(!_leftSeq.isEmpty()) {
+            sortedSequence.add(_leftSeq.pollFirst());
+        }
+
+        return sortedSequence;
+    }
+}
---
+++ src/test/java/de/oliverpabst/mergesort/MergeSortTest.java
@@ -0,0 +1,52 @@
+package de.oliverpabst.mergesort;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class MergeSortTest {
+
+    public ArrayList<Integer> getResult() {
+        ArrayList<Integer> ref = new ArrayList<>();
+        ref.add(1);
+        ref.add(2);
+        ref.add(5);
+        ref.add(7);
+        ref.add(10);
+        ref.add(15);
+        ref.add(16);
+        ref.add(17);
+
+        return ref;
+    }
+
+    public ArrayList<Integer> getTestList() {
+        ArrayList<Integer> test = new ArrayList<>();
+        test.add(1);
+        test.add(10);
+        test.add(7);
+        test.add(17);
+        test.add(16);
+        test.add(5);
+        test.add(15);
+        test.add(2);
+
+        return test;
+    }
+
+    @Test
+    public void testMergeSort() {
+        MergeSort<Integer> ms = new MergeSort(getTestList());
+
+        ArrayList<Integer> sortResult = ms.getSortedSequenceAsArrayList();
+        ArrayList<Integer> refList = getResult();
+
+        for(int i = 0; i < sortResult.size(); i++) {
+            assertEquals(sortResult.get(i), refList.get(i));
+        }
+        assertEquals(sortResult.size(), refList.size());
+    }
+}
